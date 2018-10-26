8438397f2d30d5508991a9c6c93738532f3c4ef4
matheuscampanhaf <matheuscampanhaf@gmail.com>
2018-10-26 13:38:21 -0300

    even more almost there

--- dojotmodulepython/Messenger.py
+++ dojotmodulepython/Messenger.py
@@ -14,7 +14,7 @@ class Messenger():
         self.event_callbacks = dict()
         self.tenants = []
         self.subjects = dict()
-        self.topics = []
+        self.topics = dict()
         self.producer_topics = dict()
         self.global_subjects = dict()
         self.queued_messages = []
@@ -22,6 +22,7 @@ class Messenger():
 
         self.producer = Producer()
         ret = self.producer.init()
+        
 
         if ret:
             print("Producer for module %s is ready" % self.instance_id)
@@ -32,7 +33,11 @@ class Messenger():
         else:
             print("Could not create producer")
             # TODO: process.exit()
-        self.create_channel(config.dojot['subjects']['tenancy'], "r", True)
+
+        self.consumer = Consumer("history" + str(uuid.uuid4()))
+        # self.consumer.start()
+
+        self.create_channel(config.dojot['subjects']['tenancy'], "rw", True)
 
 
 
@@ -82,7 +87,7 @@ class Messenger():
 
         self.tenants.append(data['tenant'])
         for sub in self.subjects:
-            self.__bootstrap_tenants(sub, tenant, self.subjects[sub]['mode'])
+            self.__bootstrap_tenants(sub, data['tenant'], self.subjects[sub]['mode'])
         self.emit(config.dojot['subjects']['tenancy'], config.dojot['management_service'], "new-tenant", data['tenant'])
 
 
@@ -122,10 +127,6 @@ class Messenger():
         
         if(subject not in self.subjects and subject not in self.global_subjects):
             self.create_channel(subject)
-
-        if test:
-            self.consumer.run()
-
     
 
     def create_channel(self, subject, mode="r", is_global=False):
@@ -170,10 +171,13 @@ class Messenger():
                 return
 
             print("Got topic for subject %s and tenant %s: %s" % (subject,tenant,ret_topic))
-            self.topics.append(ret_topic)
+            self.topics[ret_topic] = {"tenant": tenant, "subject": subject}
 
-            if ("passwd" in mode):
-                self.consumer = Consumer(ret_topic, lambda messages: self.__process_kafka_messages(tenant, subject, messages), "history")
+            if ("r" in mode):
+                print("Telling consumer to subscribe to new topic")
+                self.consumer.subscribe(ret_topic, self.__process_kafka_messages)
+                if(len(self.topics) == 1):
+                    self.consumer.start()
                 print("ok till here")
 
             if("w" in mode):
@@ -181,17 +185,24 @@ class Messenger():
                 if (subject not in self.producer_topics):
                     self.producer_topics[subject] = dict()
                 self.producer_topics[subject][tenant] = ret_topic
+
+            
         except Exception as error:
             print("Could not get topic: %s" % error)
 
 
 
-    def __process_kafka_messages(self,tenant, subjects, messages):
+    def __process_kafka_messages(self, topic, messages):
         """
             TODO: write something
         """
+        
+        if (topic not in self.topics):
+            print("Dont know why received message from this topic I dont have nothing to do with that.")
+            return
+
         print("[Process kafka messages] Received messages: %s" % messages)
-        self.emit(subjects, tenant, "message", messages)
+        self.emit(self.topics[topic]['subject'], self.topics[topic]['tenant'], "message", messages)
 
     def publish(self, subject, tenant, message):
         """
--- main.py
+++ main.py
@@ -4,18 +4,73 @@ from dojotmodulepython.kafka import TopicManager
 from dojotmodulepython.kafka import Producer
 from dojotmodulepython import auth
 from dojotmodulepython import Messenger
+import time
 
+
+def does_nothing(tenant,data):
+    print("[SUCCESS] received message!!! tenant: %s, data: %s" % (tenant,data))
 
 def main():
     print(config.dojot['subjects']['tenancy'])
     messenger = Messenger("Matheus")
     messenger.init()
     print("\n")
-    messenger.create_channel(config.dojot['subjects']['device_data'], "w")
+    messenger.create_channel(config.dojot['subjects']['device_data'], "rw")
+    messenger.on("device-data","message",does_nothing,True)
+    time.sleep(1)
     messenger.publish(config.dojot['subjects']['device_data'], "admin", {
         "metadata": {
             "deviceid": "c6ea4b",
             "tenant": "admin",
+            "timestamp": 1528226137452,
+            "templates": [2, 3]
+        },
+        "attrs": {
+            "humidity": 60
+        }
+    })
+    time.sleep(1)
+    print("\n")
+    messenger.publish(config.dojot['subjects']['device_data'], "admin", {
+        "metadata": {
+            "deviceid": "c6ea4b",
+            "tenant": "admin",
+            "timestamp": 1528226137452,
+            "templates": [2, 3]
+        },
+        "attrs": {
+            "humidity": 60
+        }
+    })
+    time.sleep(1)
+    print("\n")
+    messenger.publish(config.dojot['subjects']['device_data'], "admin", {
+        "metadata": {
+            "deviceid": "c6ea4b",
+            "tenant": "admin",
+            "timestamp": 1528226137452,
+            "templates": [2, 3]
+        },
+        "attrs": {
+            "humidity": 60
+        }
+    })
+    print("\n")
+
+    messenger.publish(config.dojot['subjects']['tenancy'], config.dojot['management_service'], {"tenant":"matheus"})
+    messenger.create_channel(config.dojot['subjects']['device_data'], "r")
+    messenger.on(config.dojot['subjects']['devices'], "message", does_nothing)
+    time.sleep(2)
+    print("\n")
+    messenger.create_channel(config.dojot['subjects']['tenancy'],"rw")
+    messenger.publish(config.dojot['subjects']['tenancy'], config.dojot['management_service'], {"tenant":"matheus"})
+    print("Please send that there is a new tenant")
+    print("WANNA PUBLISH ON NEW TENANT")
+    print("\n")
+    messenger.publish(config.dojot['subjects']['device_data'], "matheus", {
+        "metadata": {
+            "deviceid": "c6ea4b",
+            "tenant": "matheus",
             "timestamp": 1528226137452,
             "templates": [2, 3]
         },
